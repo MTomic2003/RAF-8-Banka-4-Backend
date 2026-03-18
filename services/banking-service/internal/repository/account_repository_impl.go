47b00c870613ed0b3794943ccad477be56013c72
PetkovicM101 <mpetkovic10123rn@raf.rs>
2026-03-19 00:06:35 +0100

    fixing funcions

--- services/banking-service/internal/repository/account_repository_impl.go
+++ services/banking-service/internal/repository/account_repository_impl.go
@@ -5,6 +5,7 @@ import (
 	"banking-service/internal/model"
 	"common/pkg/db"
 	"context"
+	"errors"
 
 	"gorm.io/gorm"
 )
@@ -33,20 +34,30 @@ func (r *accountRepository) AccountNumberExists(ctx context.Context, accountNumb
 	return count > 0, err
 }
 
-func (r *accountRepository) GetByAccountNumber(ctx context.Context, accountNumber string) (*model.Account, error) {
+func (r *accountRepository) FindByAccountNumber(ctx context.Context, accountNumber string) (*model.Account, error) {
 	db := db.DBFromContext(ctx, r.db)
 
 	var account model.Account
-	if err := db.WithContext(ctx).First(&account, accountNumber).Error; err != nil {
+	err := db.WithContext(ctx).Preload("Currency").First(&account, accountNumber).Error
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+	if err != nil {
 		return nil, err
 	}
 	return &account, nil
 }
 
-func (r *accountRepository) Update(ctx context.Context, account *model.Account) error {
+func (r *accountRepository) UpdateBalance(ctx context.Context, account *model.Account) error {
 	db := db.DBFromContext(ctx, r.db)
 
-	return db.WithContext(ctx).Save(account).Error
+	return db.WithContext(ctx).Model(account).Updates(map[string]interface{}{
+		"balance":           account.Balance,
+		"available_balance": account.AvailableBalance,
+		"daily_spending":    account.DailySpending,
+		"monthly_spending":  account.MonthlySpending,
+	}).Error
 }
 func (r *accountRepository) FindAll(ctx context.Context, query *dto.ListAccountsQuery) ([]*model.Account, int64, error) {
 	var accounts []*model.Account
